718272af2730d4bcc8b19bc80435862c01c5446c
TstevetsT <stevets@hushmail.com>
2014-04-28 16:32:00 -0700

    v1.1

--- OFDMwFading.m
+++ OFDMwFading.m
@@ -50,6 +50,8 @@ sigmaValues = [0.0, 0.02, 0.08];    %Standard Deviation
 randQ=randn(length(Symbols_Xk),1)*i;    %Creates random variations of Q
 randI=randn(length(Symbols_Xk),1);      %Creates random variations of I
 Noise=(randQ+randI)*sigmaValues;        %Imposes Noise to the signal vector
+% Add Stanford University Interium Model Fading to 
+%SUI channel   y(n)=x(n)+0.5x(n-1)+0.25x(n-2)
 
 % Add Rayleigh Fading to the Signal
 %  Rayleigh (statistical model). The effect of fading is realized as a 
@@ -59,14 +61,20 @@ Noise=(randQ+randI)*sigmaValues;        %Imposes Noise to the signal vector
 h=sqrt(abs(Noise));
 
 %% (ea) Fading Scenario 1 SingleCarrier Modulation,fading Channel
-SymbolsOut_rk_SingleCarrier(:,1)=Symbols_Xk+h(:,1)+h(:,1)*i;
-SymbolsOut_rk_SingleCarrier(:,2)=Symbols_Xk+h(:,2)+h(:,2)*i;
-SymbolsOut_rk_SingleCarrier(:,3)=Symbols_Xk+h(:,3)+h(:,3)*i;
+SymbolsOut_rk_SingleCarrier(1)=Symbols_Xk(1);
+SymbolsOut_rk_SingleCarrier(2)=Symbols_Xk(2)+0.5*SymbolsOut_rk_SingleCarrier(1);
+for i=3:N*L
+    SymbolsOut_rk_SingleCarrier(i)=Symbols_Xk(i)+0.5*Symbols_Xk(i-1)+ ...
+        0.25*Symbols_Xk(i-2);
+end
 
 %% (eb) Fading Scenario 2 Multicarrier Modulation, fading channel
-SymbolsOut_rk_woNoise(:,1)=SymbolsIFFT+h(:,1)+h(:,1)*i;
-SymbolsOut_rk_woNoise(:,2)=SymbolsIFFT+h(:,2)+h(:,2)*i;
-SymbolsOut_rk_woNoise(:,3)=SymbolsIFFT+h(:,3)+h(:,3)*i;
+SymbolsOut_rk_woNoise(1)=SymbolsIFFT(1);
+SymbolsOut_rk_woNoise(2)=SymbolsIFFT(2)+0.5*SymbolsIFFT(1);
+for i=3:N*L
+    SymbolsOut_rk_woNoise(i)=SymbolsIFFT(i)+0.5*SymbolsIFFT(i-1)+ ...
+        0.25*Symbols_Xk(i-2);
+end
 
 %% (ec) Noise Scenario 3 MultiCarrier Modulation, AWGN channel
 SymbolsOut_rk(:,1)=SymbolsIFFT+Noise(:,1);
@@ -76,9 +84,7 @@ SymbolsOut_rk(:,3)=SymbolsIFFT+Noise(:,3);
 %% (f) Use FFT to Recover Constellation Symbol
 % (fa) N/A
 % (fb) FFT Scenario 2 Multicarrier Modulation, fading channel
-SymbolsInFFT_Rayleigh(:,1) = fft(SymbolsOut_rk_woNoise(:,1));
-SymbolsInFFT_Rayleigh(:,2) = fft(SymbolsOut_rk_woNoise(:,2));
-SymbolsInFFT_Rayleigh(:,3) = fft(SymbolsOut_rk_woNoise(:,3));
+SymbolsInFFT_Rayleigh = fft(SymbolsOut_rk_woNoise);
 
 % (fc) FFT Scenario 3 MultiCarrier Modulation, AWGN channel
 SymbolsInFFT_AWGM(:,1) = fft(SymbolsOut_rk(:,1));
@@ -87,14 +93,12 @@ SymbolsInFFT_AWGM(:,3) = fft(SymbolsOut_rk(:,3));
 
 %% (g) Implement Demapping to Return Received Symbols to Bit Stream 
 % (ga) Demap Scenario 1 SingleCarrier Modulation,fading Channel
-SymbolsIn_dk_Single(:,1) = qamdemod(SymbolsOut_rk_SingleCarrier(:,1),L,0,'gray');
-SymbolsIn_dk_Single(:,2) = qamdemod(SymbolsOut_rk_SingleCarrier(:,2),L,0,'gray');
-SymbolsIn_dk_Single(:,3) = qamdemod(SymbolsOut_rk_SingleCarrier(:,3),L,0,'gray');
+SymbolsIn_dk_Single(1,:) = qamdemod(SymbolsOut_rk_SingleCarrier,L,0,...
+    'gray');
 
 % (gb) Demap Scenario 2 Multicarrier Modulation,Raleigh fading channel 
-SymbolsIn_dk_Rayleigh(:,1) = qamdemod(SymbolsInFFT_Rayleigh(:,1),L,0,'gray');
-SymbolsIn_dk_Rayleigh(:,2) = qamdemod(SymbolsInFFT_Rayleigh(:,2),L,0,'gray');
-SymbolsIn_dk_Rayleigh(:,3) = qamdemod(SymbolsInFFT_Rayleigh(:,3),L,0,'gray');
+SymbolsIn_dk_Rayleigh(1,:) = qamdemod(SymbolsInFFT_Rayleigh,L,0,...
+    'gray');
 
 % (gc) Demap Scenario 3 MultiCarrier Modulation, AWGN channel
 SymbolsIn_dk(:,1) = qamdemod(SymbolsInFFT_AWGM(:,1),L,0,'gray');
@@ -103,22 +107,12 @@ SymbolsIn_dk(:,3) = qamdemod(SymbolsInFFT_AWGM(:,3),L,0,'gray');
 
 %% (h) Convert From Integer Symbols to Binary
 %Rayleigh-Single Carrier
-DecimalVector_To_Binary_ck_Single(:,1:m) = de2bi(SymbolsIn_dk_Single(:,1),m);
-DecimalVector_To_Binary_ck_Single(:,m+1:2*m) = de2bi(SymbolsIn_dk_Single(:,2),m);
-DecimalVector_To_Binary_ck_Single(:,2*m+1:3*m) = de2bi(SymbolsIn_dk_Single(:,3),m);
-
-bitsIn_ck_Single(:,1)=reshape(DecimalVector_To_Binary_ck_Single(:,1:m),[N*L*m 1]);
-bitsIn_ck_Single(:,2)=reshape(DecimalVector_To_Binary_ck_Single(:,m+1:2*m),[N*L*m 1]);
-bitsIn_ck_Single(:,3)=reshape(DecimalVector_To_Binary_ck_Single(:,2*m+1:3*m),[N*L*m 1]);
+DecimalVector_To_Binary_ck_Single = de2bi(SymbolsIn_dk_Single(:),m);
+bitsIn_ck_Single=reshape(DecimalVector_To_Binary_ck_Single,[N*L*m 1]);
 
 %Rayleigh-MultiCarrier
-DecimalVector_To_Binary_ck_Rayleigh(:,1:m) = de2bi(SymbolsIn_dk_Rayleigh(:,1),m);
-DecimalVector_To_Binary_ck_Rayleigh(:,m+1:2*m) = de2bi(SymbolsIn_dk_Rayleigh(:,2),m);
-DecimalVector_To_Binary_ck_Rayleigh(:,2*m+1:3*m) = de2bi(SymbolsIn_dk_Rayleigh(:,3),m);
-
-bitsIn_ck_Rayleigh(:,1)=reshape(DecimalVector_To_Binary_ck_Rayleigh(:,1:m),[N*L*m 1]);
-bitsIn_ck_Rayleigh(:,2)=reshape(DecimalVector_To_Binary_ck_Rayleigh(:,m+1:2*m),[N*L*m 1]);
-bitsIn_ck_Rayleigh(:,3)=reshape(DecimalVector_To_Binary_ck_Rayleigh(:,2*m+1:3*m),[N*L*m 1]);
+DecimalVector_To_Binary_ck_Rayleigh = de2bi(SymbolsIn_dk_Rayleigh,m);
+bitsIn_ck_Rayleigh=reshape(DecimalVector_To_Binary_ck_Rayleigh,[N*L*m 1]);
 
 %AWGM-MultiCarrier
 DecimalVector_To_Binary_ck(:,1:m) = de2bi(SymbolsIn_dk(:,1),m);
@@ -130,71 +124,34 @@ bitsIn_ck(:,2)=reshape(DecimalVector_To_Binary_ck(:,m+1:2*m),[N*L*m 1]);
 bitsIn_ck(:,3)=reshape(DecimalVector_To_Binary_ck(:,2*m+1:3*m),[N*L*m 1]);
 
 %% (i) Calculate Bit Error Rates and display
-fprintf('\nL = %d     N = %d\n',L,N)
-fprintf('\n Sigma = 0.0\n')
-[numErrors,ber]=biterr(bits_bk,bitsIn_ck_Single(:,1));
-fprintf('\nThe bit error rate = %5.2e, based on %d errors for single carrier fading.\n', ber,numErrors)
-[numErrors,ber]=biterr(bits_bk,bitsIn_ck_Rayleigh(:,1));
-fprintf('\nThe bit error rate = %5.2e, based on %d errors for multiple carrier fading.\n', ber,numErrors)
+fprintf('\n%d Bits Per Symbol over %d samples resulted in the following:\n',L,N)
+[numErrors,ber]=biterr(bits_bk,bitsIn_ck_Single);
+fprintf('Single Carrier Stanford Model had a bit error rate of %5.2e(%d errors).\n' , ber,numErrors)
+[numErrors,ber]=biterr(bits_bk,bitsIn_ck_Rayleigh);
+fprintf('Multiple Carrier Stanford Model had a bit error rate of %5.2e(%d errors).\n', ber,numErrors)
 [numErrors,ber]=biterr(bits_bk,bitsIn_ck(:,1));
-fprintf('\nThe bit error rate = %5.2e, based on %d errors for AWGN.\n', ber,numErrors)
-
-fprintf('\n Sigma = 0.02\n')
-[numErrors,ber]=biterr(bits_bk,bitsIn_ck_Single(:,2));
-fprintf('\nThe bit error rate = %5.2e, based on %d errors for single carrier fading.\n', ber,numErrors)
-[numErrors,ber]=biterr(bits_bk,bitsIn_ck_Rayleigh(:,2));
-fprintf('\nThe bit error rate = %5.2e, based on %d errors for multiple carrier fading.\n', ber,numErrors)
+fprintf('Multiple Carrier AWGN had a bit error rate of %5.2e(%d errors) with Sigma=0.\n', ber,numErrors)
 [numErrors,ber]=biterr(bits_bk,bitsIn_ck(:,2));
-fprintf('\nThe bit error rate = %5.2e, based on %d errors for AWGN.\n', ber,numErrors)
-
-fprintf('\n Sigma = 0.08\n')
-[numErrors,ber]=biterr(bits_bk,bitsIn_ck_Single(:,3));
-fprintf('\nThe bit error rate = %5.2e, based on %d errors for single carrier fading.\n', ber,numErrors)
-[numErrors,ber]=biterr(bits_bk,bitsIn_ck_Rayleigh(:,3));
-fprintf('\nThe bit error rate = %5.2e, based on %d errors for multiple carrier fading.\n', ber,numErrors)
+fprintf('Multiple Carrier AWGN had a bit error rate of %5.2e(%d errors) with Sigma=0.02.\n', ber,numErrors)
 [numErrors,ber]=biterr(bits_bk,bitsIn_ck(:,3));
-fprintf('\nThe bit error rate = %5.2e, based on %d errors for AWGN.\n', ber,numErrors)
+fprintf('Multiple Carrier AWGN had a bit error rate of %5.2e(%d errors) with Sigma=0.08.\n', ber,numErrors)
 
 %% (j) Graphs
-splotfig1=scatterplot(SymbolsOut_rk_SingleCarrier(:,1),1,0,'g.');
+splotfig1=scatterplot(SymbolsOut_rk_SingleCarrier,1,0,'g.');
 hold on
-%scatterplot(Symbols_Xk,1,0,'k*',splotfig1);
-title(strcat('Single Carrier: L = ', {' '},num2str(L),' sigma = 0.0'))
+scatterplot(Symbols_Xk,1,0,'k*',splotfig1);
+title(strcat('Single Carrier: L = ', {' '},num2str(L)))
 axis([-m m -m m])
 
-splotfig2=scatterplot(SymbolsOut_rk_SingleCarrier(:,1),1,0,'g.');
+splotfig4=scatterplot(SymbolsInFFT_Rayleigh,1,0,'g.');
 hold on
-%scatterplot(Symbols_Xk,1,0,'k*',splotfig2);
-title(strcat('Single Carrier: L = ', {' '},num2str(L),' sigma = 0.02'))
-axis([-m m -m m])
-
-splotfig3=scatterplot(SymbolsOut_rk_SingleCarrier(:,1),1,0,'g.');
-hold on
-%scatterplot(Symbols_Xk,1,0,'k*',splotfig3);
-title(strcat('Single Carrier: L = ', {' '},num2str(L),' sigma = 0.08'))
-axis([-m m -m m])
-
-splotfig4=scatterplot(SymbolsInFFT_Rayleigh(:,1),1,0,'g.');
-hold on
-%scatterplot(Symbols_Xk,1,0,'k*',splotfig4);
-title(strcat('Multiple Carrier: L = ', {' '},num2str(L),' sigma = 0.0'))
-axis([-m m -m m])
-
-splotfig5=scatterplot(SymbolsInFFT_Rayleigh(:,2),1,0,'g.');
-hold on
-scatterplot(Symbols_Xk,1,0,'k*',splotfig5);
-title(strcat('Multiple Carrier: L = ', {' '},num2str(L),' sigma = 0.02'))
-axis([-m m -m m])
-
-splotfig6=scatterplot(SymbolsInFFT_Rayleigh(:,3),1,0,'g.');
-hold on
-scatterplot(Symbols_Xk,1,0,'k*',splotfig6);
-title(strcat('Multiple Carrier: L = ', {' '},num2str(L),' sigma = 0.08'))
+scatterplot(Symbols_Xk,1,0,'k*',splotfig4);
+title(strcat('Multiple Carrier: L = ', {' '},num2str(L)))
 axis([-m m -m m])
 
 splotfig7=scatterplot(SymbolsInFFT_AWGM(:,1),1,0,'g.');
 hold on
-%scatterplot(Symbols_Xk,1,0,'k*',splotfig7);
+scatterplot(Symbols_Xk,1,0,'k*',splotfig7);
 title(strcat('Multiple Carrier AWGN: L = ', {' '},num2str(L),' sigma = 0.0'))
 axis([-m m -m m])
 
@@ -202,7 +159,7 @@ splotfig8=scatterplot(SymbolsInFFT_AWGM(:,2),1,0,'g.');
 hold on
 scatterplot(Symbols_Xk,1,0,'k*',splotfig8);
 title(strcat('Multiple Carrier AWGN: L = ', {' '},num2str(L),' sigma = 0.02'))
-axis([-m m -m m])
+axis([-m-1 m+1 -m-1 m+1])
 
 splotfig9=scatterplot(SymbolsInFFT_AWGM(:,3),1,0,'g.');
 hold on
